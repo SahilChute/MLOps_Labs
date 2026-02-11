c89afba5f2e27478a5231a89e74aa6953f728cc3
Sahil Chute <sahil.chute1103@gmail.com>
2026-02-11 12:39:16 -0500

    submission

--- lab2/terraform-lab-aws/main.tf
+++ lab2/terraform-lab-aws/main.tf
@@ -2,6 +2,7 @@ provider "aws" {
   region = "us-west-2"
 }
 
+# Create a VPC
 resource "aws_vpc" "lab_vpc" {
   cidr_block = "10.1.0.0/16"
 
@@ -10,6 +11,7 @@ resource "aws_vpc" "lab_vpc" {
   }
 }
 
+# Create a subnet inside the VPC
 resource "aws_subnet" "lab_subnet" {
   vpc_id     = aws_vpc.lab_vpc.id
   cidr_block = "10.1.1.0/24"
@@ -19,6 +21,7 @@ resource "aws_subnet" "lab_subnet" {
   }
 }
 
+# Launch an EC2 instance inside the subnet
 resource "aws_instance" "lab_ec2" {
   ami           = "ami-08d70e59c07c61a3a" # Amazon Linux 2 (us-west-2)
   instance_type = "t3.micro"
@@ -27,4 +30,17 @@ resource "aws_instance" "lab_ec2" {
   tags = {
     Name = "lab-ec2-instance"
   }
+}
+
+# Outputs to show resource IDs
+output "ec2_instance_id" {
+  value = aws_instance.lab_ec2.id
+}
+
+output "subnet_id" {
+  value = aws_subnet.lab_subnet.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.lab_vpc.id
 }
